9a8cc180bc19c5cd9d6883c6844e242c304d37a7
peter miller <petermiller1986@gmail.com>
2014-05-06 17:21:25 +0930

    more instructions

--- get_my_external_ip_address.sh
+++ get_my_external_ip_address.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# this script gets the external ip address of the current server
+# this script gets the external ip address of the current server.
 # it should be run on the network which is to be accessed from the internet
-# (eg the webserver or ssh server)
+# (ie run it on the webserver or ssh server machine)
+
+# on debian/ubuntu, run it automatically once every minute by putting the
+# following entry in /etc/crontab:
+# */1 * * * * root get_my_external_ip_address.sh
 
 # -q = quiet
 # -O = output file
