0e2b3240ceba754024803386074dc5e959764949
peter miller <petermiller1986@gmail.com>
2014-05-07 16:30:49 +0930

    use php sha1 since openssl sha1 givs odd results

--- server.sh
+++ server.sh
@@ -11,14 +11,19 @@ pw="a long string of random characters $%^$%^(f@S!<>"
 # the salt is the current unixtime - the intermediary will only accept each salt
 # once
 salt=$(date +%s)
-hex_hash=$(echo -n "$salt$pw" | openssl sha1 -hmac "key")
+
+# use the php sha1 function to simply match the intermediary result
+hex_hash=$(echo "<?php echo sha1('$salt$pw'); ?>" | php)
 
 # some versions of openssl prepend the string, "(stdin)= " - strip this off if
 # it exists
 hex_hash=$(echo $hex_hash | sed "s/^.* //")
 
 # make sure to use a https intermediary to avoid man-in-the-middle attacks
-wget -q -O- "https://intermediary.com/mulll-dynamic-dns/intermediary_ip.php?"\
-"action=update&salt=$salt&hash=$hex_hash"
+url="https://intermediary.com/mulll-dynamic-dns/intermediary_ip.php?action="\
+"update&salt=$salt&hash=$hex_hash"
+
+# remove the -q flag and run this script manually (./server.sh) to debug
+wget -qO /tmp/mulll-dynamic-dns.log "$url"
 
 exit $?
